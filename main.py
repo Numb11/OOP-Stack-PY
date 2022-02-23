dac16fd50dd3237e9d5c9c8393a14ae109b6f00d
Soup <87945079+Numb11@users.noreply.github.com>
2022-02-23 14:48:54 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,24 @@
 class stack:
-    def __init__(self,length,stack):
+    def __init__(self,length,stack): 
       self.length = length
       self.stack = stack
 
     def pop(self):
      if self.is_empty():
          del self.stack[-1]
-     else:
+     else:  #More error handling-------
          print("Stack under flow error --------------")
     def is_empty(self):
-     return (len(self.stack) > 0)
+     return (len(self.stack) > 0) #Using the python in-built function len to act as a pointer
 
     def peek(self):
      return self.stack[-1]
 
-    def push(self,item):
+    def push(self,item):   
       if (self.is_empty()) and (self.is_full()):
          (self.stack).append(item)
-      elif not(self.is_empty()):
-         print("Stack under flow error --------------")
+      elif not(self.is_empty()):  #More error handling-------
+         print("Stack under flow error --------------")  
       elif not(self.is_full()):
          print("Stack over flow error --------------")
 
@@ -29,9 +29,10 @@ class stack:
         return self.stack
 
 #Class over
-length = int(input("Enter length of stack:"))
+length = int(input("Enter length of stack:")) #Relevant data------
 listy = input("Enter stack seperated by spaces:").split()
-if not(length<=len(listy)):
+if not(length<=len(listy)):  #Error handling
+#Testing methods required to use a stack effectively
  stack1 = stack(length,listy)
  stack1.pop()
  print(stack1.describe_stack())
